27112bb2f7ba8f04afb665556556db7d31c03db1
MErenAydin <merenep12@gmail.com>
2021-06-01 14:36:51 +0000

    Test commit for webhook 2

--- incubator.py
+++ incubator.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/pullNewVersion', methods=['POST'])
 def pullNewVersion():
-    request_data = request.get_data()
-    print(request_data)
+    request_data = request.get_json()
+    print(json.dumps(request_data, indent=4, sort_keys=True))
 
 @app.route('/')
 def index():
@@ -22,3 +22,4 @@ def humidity():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
