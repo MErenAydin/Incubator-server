d341686d1e8a4f88a71ec5f80e8bfdbae0d1a064
MErenAydin <merenep12@gmail.com>
2021-06-01 11:15:39 +0000

    Core files added

---
+++ incubator.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/temperature')
+def temperature():
+    return "37.75"
+
+@app.route('/humidity')
+def humidity():
+    return "58.65"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
