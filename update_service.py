a1263b4cd13d07971a61a121235fcc3699c9a2e3
Muhammet Eren Aydın <merenep12@gmail.com>
2021-06-03 15:33:04 +0300

    Added try except blocks

--- update_service.py
+++ update_service.py
@@ -13,13 +13,19 @@ process = object()
 @app.route('/pullNewVersion', methods=['POST'])
 def pullNewVersion():
     global process
-    request_data = request.get_json()
-    print(json.dumps(request_data, indent=4, sort_keys=True))
-    process.terminate()
-    subprocess.Popen(["git", "pull"], shell=False).wait()
-    process = subprocess.Popen(["python3", file_path], shell=False)
+    try:
+        request_data = request.get_json()
+        print(json.dumps(request_data, indent=4, sort_keys=True))
+        process.terminate()
+        subprocess.Popen(["git", "pull"], shell=False).wait()
+        process = subprocess.Popen(["python3", file_path], shell=False)
+    except Exception as e:
+        print(str(e))
 
 if __name__ == '__main__':
-    subprocess.Popen(["git", "pull"], cwd = path, shell=False).wait()
-    process = subprocess.Popen(["python3", file_path], shell=False)
+    try:
+        subprocess.Popen(["git", "pull"], cwd = path, shell=False).wait()
+        process = subprocess.Popen(["python3", file_path], shell=False)
+    except Exception as e:
+        print(str(e))
     app.run(host='0.0.0.0', port=8080)
